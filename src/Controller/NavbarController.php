aea3b53065fa26988b6ee329a68d2ff6dcfe97d7
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2023-04-05 15:11:50 +0200

    Retrieve movies from database

--- src/Controller/NavbarController.php
+++ src/Controller/NavbarController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 class NavbarController extends AbstractController
 {
-    public function __invoke(string $mainRequestPath): Response
+    public function __invoke(string $mainRequestPath, MovieRepository $movieRepository): Response
     {
+        $movies = $movieRepository->findAll();
+
         return $this->render('_navbar.html.twig', [
-            'movies' => MovieDetailsController::MOVIES,
+            'movies' => $movies,
             'main_request_path' => $mainRequestPath,
         ]);
     }
