ba52e4abce4ab2363548012da399e4fd024a5160
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2023-04-05 15:30:54 +0200

    Add latest movies on homepage

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MovieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(MovieRepository $movieRepository): Response
+    {
+        $latestMovies = $movieRepository->findLatest();
+
+        return $this->render('home/index.html.twig', [
+            'latest_movies' => $latestMovies,
+        ]);
+    }
+}
